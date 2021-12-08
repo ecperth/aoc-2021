b151a9b834f0f75d4d24b639b642a6db671e05bb
Eoghan <eoghanc@familyzone.com>
2021-12-08 16:13:04 +0800

    day 3

---
+++ questions/day3/day3.go
@@ -0,0 +1,117 @@
+package day3
+
+import (
+	util "adventOfCode/util"
+	"fmt"
+	"math"
+	"strconv"
+)
+
+func Solve() {
+	inputStr, err := util.ScanLines("input/day3.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Part 1: " + strconv.Itoa(part1(inputStr)))
+	fmt.Println("Part 2: " + strconv.Itoa(part2(inputStr)))
+}
+
+func part1(input []string) int {
+
+	rowCount := len(input)
+	bitLength := len(input[0])
+
+	bitCounts := make([]int, bitLength)
+	for i := range bitCounts {
+		bitCounts[i] = 0
+	}
+
+	for i := 0; i < rowCount; i++ {
+		for j, bit := range input[i] {
+			if byte(bit) == '1' {
+				bitCounts[j]++
+			}
+		}
+	}
+
+	gammaRate := 0
+	epsilonRate := 0
+	for i, bit := range bitCounts {
+		if float64(bit)/float64(rowCount) > 0.5 {
+			gammaRate += int(math.Pow(float64(2), float64(bitLength-(i+1))))
+		} else {
+			epsilonRate += int(math.Pow(float64(2), float64(bitLength-(i+1))))
+		}
+	}
+
+	return gammaRate * epsilonRate
+}
+
+func part2(input []string) int {
+
+	bitLength := len(input[0])
+
+	generatorRatings := getEntriesWithMostOrLeastCommonBitAtIndex(input, 0, true)
+	scrubberRatings := getEntriesWithMostOrLeastCommonBitAtIndex(input, 0, false)
+	index := 1
+
+	for len(generatorRatings) > 1 {
+		generatorRatings = getEntriesWithMostOrLeastCommonBitAtIndex(generatorRatings, index, true)
+		index = index + 1
+	}
+
+	index = 1
+	for len(scrubberRatings) > 1 {
+		scrubberRatings = getEntriesWithMostOrLeastCommonBitAtIndex(scrubberRatings, index, false)
+		index = index + 1
+	}
+
+	generatorRating := 0
+	scrubberRating := 0
+	for i, bit := range generatorRatings[0] {
+		if byte(bit) == '1' {
+			generatorRating += int(math.Pow(float64(2), float64(bitLength-(i+1))))
+		}
+	}
+
+	for i, bit := range scrubberRatings[0] {
+		if byte(bit) == '1' {
+			scrubberRating += int(math.Pow(float64(2), float64(bitLength-(i+1))))
+		}
+	}
+
+	return generatorRating * scrubberRating
+}
+
+func getEntriesWithMostOrLeastCommonBitAtIndex(input []string, index int, mostCommon bool) []string {
+
+	rowCount := len(input)
+	bitCount := 0
+	desiredBit := '1'
+	var returnArray []string
+
+	for i := 0; i < rowCount; i++ {
+		if input[i][index] == '1' {
+			bitCount++
+		}
+	}
+
+	if mostCommon {
+		if float64(bitCount)/float64(rowCount) < 0.5 {
+			desiredBit = '0'
+		}
+	} else {
+		if float64(bitCount)/float64(rowCount) >= 0.5 {
+			desiredBit = '0'
+		}
+	}
+
+	for i := 0; i < rowCount; i++ {
+		if input[i][index] == byte(desiredBit) {
+			returnArray = append(returnArray, input[i])
+		}
+	}
+
+	return returnArray
+}
--- questions/questions.go
+++ questions/questions.go
@@ -3,6 +3,7 @@ package questions
 import (
 	"adventOfCode/2021/questions/day1"
 	"adventOfCode/2021/questions/day2"
+	"adventOfCode/2021/questions/day3"
 )
 
 func Solve(day int) {
@@ -12,5 +13,7 @@ func Solve(day int) {
 		day1.Solve()
 	case 2:
 		day2.Solve()
+	case 3:
+		day3.Solve()
 	}
 }
